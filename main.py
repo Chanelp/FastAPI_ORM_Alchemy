2767fe95abe7389283204355469c0f585a3b2ecc
chanelp <mariannissanchez27@gmail.com>
2023-11-11 17:44:41 -0400

    Consulta de datos port categoria

--- main.py
+++ main.py
@@ -88,14 +88,22 @@ def get_movie(id: int =  Path(ge=1, le=200)) -> Movie:
             return JSONResponse(status_code = status.HTTP_404_NOT_FOUND, content= {"message": "No encontrada"})
         
         return JSONResponse(status_code= status.HTTP_200_OK, content = jsonable_encoder(result))
-    
+
 @app.get(path= "/movies/", summary="Get movie by category", tags = ["Movies"], response_model = List[Movie], status_code= status.HTTP_200_OK)
 def get_movie_by_category(category: str = Query(min_length= 5, max_length= 15)) -> List[Movie]:
     try:
-        data = [ movies for movies in movies if movies['category'] == category ]
-        return JSONResponse(content= data, status_code= status.HTTP_200_OK)
-    except IndexError:
-        return {"Error": "Movies in that category not found!"}
+        db = Session()
+    except HTTPException as e:
+        raise HTTPException(status_code= status.HTTP_500_INTERNAL_SERVER_ERROR, detail = str(e))
+    
+    else:
+        result = db.query(MovieModel).filter(MovieModel.category == category).all()
+
+        if not result:
+            return JSONResponse(status_code = status.HTTP_404_NOT_FOUND, content = {"Error": "Movies in that category not found!"})
+        
+        return JSONResponse(status_code =  status.HTTP_200_OK, content = jsonable_encoder(result))
+        
     
 @app.post(path = "/movies", tags = ["Movies"], summary= "Add a new movie to films", response_model= dict, status_code=status.HTTP_201_CREATED)
 async def register_movie(new_movie: Movie) -> dict:
