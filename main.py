6d79bd4847c2d729970f9392ee87348c76403b42
chanelp <mariannissanchez27@gmail.com>
2023-11-11 20:30:14 -0400

    Creación de routers: modulando la aplicación

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from jwt_manager import create_token
 from config.database import Base, engine
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
+from routers.user import login_router
 
 app = FastAPI()
 app.title = "First Application Programming Interface with FastApi"
@@ -11,5 +12,6 @@ app.version = "0.0.2"
 
 app.add_middleware(ErrorHandler)
 app.include_router(movie_router)
+app.include_router(login_router)
 
 Base.metadata.create_all(bind = engine)
