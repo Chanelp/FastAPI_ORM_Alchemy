8b2c37dfd5d31e856ece9e49bb100c11a99a30d9
chanelp <mariannissanchez27@gmail.com>
2023-11-18 12:02:27 -0400

    Deploy

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from config.database import Base, engine
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import login_router
+import uvicorn
+import os
 
 app = FastAPI()
 app.title = "First Application Programming Interface with FastApi"
@@ -14,3 +16,7 @@ app.include_router(movie_router)
 app.include_router(login_router)
 
 Base.metadata.create_all(bind = engine)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
